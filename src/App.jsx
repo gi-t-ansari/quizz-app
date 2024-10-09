5c8a0ef057df0cd252ad85762ba751c4d8629900
gi-t-ansari <tanzeelshamsh.in@gmail.com>
2024-10-09 15:40:46 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <h1 className="text-red-500">Quiz app</h1>;
+}
+
+export default App;
